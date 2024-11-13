2f2f004a96a85a7f9e7036de8461071450889698
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-11-13 09:44:56 +0700

    #3 Struktur Folder

---
+++ Laravel_11/resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman About</h1>
+    <h3>Owner: <? echo $nama; ?></h3>
+    <script src="js/script.js"></script>
+    <img src="img/suga.jpg.jpg" alt="" width="200"/>
+</body>
+</html>
