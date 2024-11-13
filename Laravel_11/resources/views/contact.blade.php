d6e704c739b9f49d7e587641569e520390deff66
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-11-13 13:07:57 +0700

    #4 Blade templating engine

---
+++ Laravel_11/resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+        Instagram: <a href="http://instagram.com/septiyani_nisa">Instagram</a    
+        </li>
+
+        <li>
+            Instagram: <a href="http://instagram.com/min_923">Instagram</a    
+            </li>
+    </ul>
+</body>
+</html>
