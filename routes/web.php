e644bf3629109ff7838e2fc9c8c326af608142c8
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-11-23 17:20:52 +0700

    #11 Eloquent Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,13 @@ Route::get('/posts/{post:slug}', function(Post$post){
 return view('posts',['title' => 'Single Post', 'posts' => $post]);
 
 });
+
+Route::get('/authors/{user}', function(User $user){
+    
+
+    return view('posts',['title' => 'Articles by'. $user->name, 'posts' => $user->posts]);
+    
+    });
 
 Route::get('/contact', function () {
     return view('contact',['title'=>'Contact']);
