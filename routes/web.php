01d92b59e6189c52ccf4360fe5ca19a442605139
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-11-24 19:45:57 +0700

    #14 N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Route::get('/about', function () {
 
 
 Route::get('/posts', function () {
-    return view('posts',['title'=>'Blog','posts'=>Post::all()]);
+    //$posts = Post::with(['author','category'])->latest()->get();
+    $posts = Post::latest()->get();
+    return view('posts',['title'=>'Blog','posts'=> $posts]);
 });
 
 Route::get('/posts/{post:slug}', function(Post$post){
@@ -30,14 +32,16 @@ return view('posts',['title' => 'Single Post', 'posts' => $post]);
 });
 
 Route::get('/authors/{user:username}', function(User $user){
-    
+    $posts = $user->posts->load('category','author');
 
-    return view('posts',['title' => count($user->posts). 'Articles by'. $user->name, 'posts' => $user->posts]);
+    return view('posts',['title' => count($posts). 'Articles by'. 
+    $user->name, 'posts' => $posts]);
     
     });
 
     Route::get('/categories/{category:slug}', function(Category $category){
     
+       // $posts = $category->posts->load('category','author');
 
         return view('posts',['title' => 'Articles in:'. $category->name, 'posts' => $category->posts]);
         
